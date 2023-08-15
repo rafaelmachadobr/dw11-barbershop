e231a816c9bfbc25c053fbbf727c312bdcd17217
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-08-15 13:35:06 -0300

    feat: add Splash Page

---
+++ dw_barbershop/lib/src/features/splash/splash_page.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  var _scale = 10.0;
+  var _animationOpacityLogo = 0.0;
+
+  double get _logoAnimationWidth => 100 * _scale;
+  double get _logoAnimationHeight => 120 * _scale;
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      setState(() {
+        _animationOpacityLogo = 1.0;
+        _scale = 1;
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: DecoratedBox(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(
+              'assets/images/background_image_chair.jpg',
+            ),
+            opacity: 0.2,
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: AnimatedOpacity(
+            duration: const Duration(seconds: 3),
+            curve: Curves.easeIn,
+            opacity: _animationOpacityLogo,
+            child: AnimatedContainer(
+              duration: const Duration(seconds: 3),
+              width: _logoAnimationWidth,
+              height: _logoAnimationHeight,
+              curve: Curves.linearToEaseOut,
+              child: Image.asset(
+                'assets/images/imgLogo.png',
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
